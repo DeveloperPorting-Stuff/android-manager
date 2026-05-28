a9ca6ef196bc62275b8f346b90d87a348cca6f6d
JoseXD2 <nenejose879@gmail.com>
2026-05-27 21:14:28 -0300

    build informations

--- src/androidmanager/os/Build.hx
+++ src/androidmanager/os/Build.hx
@@ -3,14 +3,45 @@ package androidmanager.os;
 #if android
 import androidmanager.jni.JNICache;
 #end
+
+/**
+ * Information about the current build, extracted from system properties.
+ */
 class Build {
     
+    /**
+     * The end-user-visible name for the end product.
+     */
     public static var MODEL(get, never):String;
+
+    /**
+     * The manufacturer of the product/hardware.
+     */
     public static var MANUFACTURER(get, never):String;
+
+    /**
+     * The name of the industrial design.
+     */
     public static var DEVICE(get, never):String;
+
+    /**
+     * The name of the underlying board, like "goldfish".
+     */
     public static var BOARD(get, never):String;
+
+    /**
+     * The consumer-visible brand with which the product/hardware will be associated, if any.
+     */
     public static var BRAND(get, never):String;
+
+    /**
+     * The name of the hardware (from the kernel command line or /proc).
+     */
     public static var HARDWARE(get, never):String;
+
+    /**
+     * A build ID string meant for displaying to the user.
+     */
     public static var DISPLAY(get, never):String;
 
     private static function get_MODEL():String {
@@ -70,6 +101,54 @@ class Build {
     private static function get_DISPLAY():String {
         #if android
         final field:Null<Dynamic> = JNICache.getStaticField('android/os/Build', 'DISPLAY', 'Ljava/lang/String;');
+        return field != null ? field.get() : "Unknown";
+        #else
+        return "Unknown";
+        #end
+    }
+}
+
+/**
+ * Various version strings and numbers extracted from system properties.
+ */
+class VERSION {
+    
+    /**
+     * The base OS build the product is based on.
+     */
+    public static var BASE_OS(get, never):String;
+
+    /**
+     * The user-visible version string (e.g., "15", "14", "13").
+     */
+    public static var RELEASE(get, never):String;
+
+    /**
+     * The SDK version of the software currently running on this hardware device.
+     */
+    public static var SDK_INT(get, never):Int;
+
+    private static function get_RELEASE():String {
+        #if android
+        final field:Null<Dynamic> = JNICache.getStaticField("android/os/Build$VERSION", "RELEASE", "Ljava/lang/String;");
+        return field != null ? field.get() : "Unknown";
+        #else
+        return "Unknown";
+        #end
+    }
+
+    private static function get_SDK_INT():Int {
+        #if android
+        final field:Null<Dynamic> = JNICache.getStaticField("android/os/Build$VERSION", "SDK_INT", "I");
+        return field != null ? field.get() : 0;
+        #else
+        return 0;
+        #end
+    }
+
+    private static function get_BASE_OS():String {
+        #if android
+        final field:Null<Dynamic> = JNICache.getStaticField("android/os/Build$VERSION", "BASE_OS", "Ljava/lang/String;");
         return field != null ? field.get() : "Unknown";
         #else
         return "Unknown";
